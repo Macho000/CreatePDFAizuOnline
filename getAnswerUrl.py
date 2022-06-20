652ca791ac9b28d00a35b4230fab684c0cac828a
Macho000 <saburotyan758@gmail.com>
2022-06-20 13:38:00 +0900

    20220620 change code

--- getAnswerUrl.py
+++ getAnswerUrl.py
@@ -10,14 +10,16 @@ def createPdf(contest: int):
   html = requests.get(answer_url)
   soup = BeautifulSoup(html.content, "html.parser")    # HTMLを解析する
   for task in ["a", "b", "c", "d", "e", "f", 'g', 'h']:
-    for element in soup.find(href=f"/contests/abc{contest}/tasks/abc{contest}_{task}"):
-      element = element.findNext('ul')
-      a = element.find('a', href=True)
-      get_problem_command = f"bash getProblem.sh {contest} {task}"
-      os.system(get_problem_command)
-      get_answer_command = f"bash getAnswer.sh {contest} {task} {a['href']}"
-      os.system(get_answer_command)
+    element = soup.find(href=f"/contests/abc{contest}/tasks/abc{contest}_{task}")
+    if element is None:
+      break
+    element = element.findNext('ul')
+    a = element.find('a', href=True)
+    get_problem_command = f"bash getProblem.sh {contest} {task}"
+    os.system(get_problem_command)
+    get_answer_command = f"bash getAnswer.sh {contest} {task} {a['href']}"
+    os.system(get_answer_command)
 
 if __name__=='__main__':
-  for contest in range(254,0, -1):
+  for contest in range(207,199, -1):
     createPdf(contest)
